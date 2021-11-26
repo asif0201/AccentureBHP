1cca168d1d45f65bb2cca70eb55ef55a816e5eaf
Asif <admin.asif@gmail.com>
2021-11-26 09:36:44 +0000

    Added terraform manifests here

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "workspace" {
+  ami           = "ami-0108d6a82a783b352"
+  instance_type = "t2.small"
+  key_name = "Key_Asif"
+  tags = {
+    Name = "Manual"
+  }
+}
